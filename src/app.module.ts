8155f8e23a3a19f4db2de1df46e3f2161772a60c
jailene14 <jailenemolina08@gmail.com>
2023-05-27 11:50:10 -0500

    .electivatercercorte

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal:true
+  }) ,TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    entities: [User],
+    synchronize: true,
+  }) ,UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
